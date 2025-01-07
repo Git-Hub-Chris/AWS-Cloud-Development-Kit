482721040ba1bbd801401a1c0a0045f004140e08
Hassan Azhar Khan <hassanazharkhan@gmail.com>
2025-01-08 03:51:48 +0500

    feat(aws-rds): support `SecondsUntilAutoPause` for serverless instance

--- packages/aws-cdk-lib/aws-rds/lib/cluster.ts
+++ packages/aws-cdk-lib/aws-rds/lib/cluster.ts
@@ -89,6 +89,17 @@ interface DatabaseClusterBaseProps {
   readonly serverlessV2MinCapacity?: number;
 
   /**
+   * The number of seconds until a serverless cluster is paused.
+   *
+   * This setting specifies the duration of inactivity in seconds after which the serverless cluster will be paused.
+   * The value must be between 300 (5 minutes) and 86400 (24 hours).
+   *
+   * @default 300
+   * @see https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/aurora-serverless-v2-auto-pause.html
+   */
+  readonly secondsUntilAutoPause?: Duration;
+
+  /**
    * What subnets to run the RDS instances in.
    *
    * Must be at least 2 subnets in two different AZs.
@@ -723,6 +734,10 @@ abstract class DatabaseClusterNew extends DatabaseClusterBase {
 
   protected readonly serverlessV2MinCapacity: number;
   protected readonly serverlessV2MaxCapacity: number;
+  /**
+   * The number of seconds until a serverless cluster is paused.
+   */
+  protected readonly secondsUntilAutoPause: Duration;
 
   protected hasServerlessInstance?: boolean;
   protected enableDataApi?: boolean;
@@ -750,6 +765,8 @@ abstract class DatabaseClusterNew extends DatabaseClusterBase {
 
     this.serverlessV2MaxCapacity = props.serverlessV2MaxCapacity ?? 2;
     this.serverlessV2MinCapacity = props.serverlessV2MinCapacity ?? 0.5;
+    this.secondsUntilAutoPause = props.secondsUntilAutoPause ?? Duration.minutes(5);
+
     this.validateServerlessScalingConfig();
 
     this.enableDataApi = props.enableDataApi;
@@ -903,6 +920,7 @@ abstract class DatabaseClusterNew extends DatabaseClusterBase {
             return {
               minCapacity: this.serverlessV2MinCapacity,
               maxCapacity: this.serverlessV2MaxCapacity,
+              secondsUntilAutoPause: this.secondsUntilAutoPause,
             };
           }
           return undefined;
@@ -1123,8 +1141,13 @@ abstract class DatabaseClusterNew extends DatabaseClusterBase {
     if (!regexp.test(this.serverlessV2MaxCapacity.toString()) || !regexp.test(this.serverlessV2MinCapacity.toString())) {
       throw new Error('serverlessV2MinCapacity & serverlessV2MaxCapacity must be in 0.5 step increments, received '+
       `min: ${this.serverlessV2MaxCapacity}, max: ${this.serverlessV2MaxCapacity}`);
-
     }
+
+    const autoPauseSeconds = this.secondsUntilAutoPause.toSeconds();
+    if (autoPauseSeconds < 300 || autoPauseSeconds > 86400) {
+      throw new Error(`secondsUntilAutoPause must be >= 300 & <= 86400, received ${autoPauseSeconds}`);
+    }
+
   }
 
   /**
